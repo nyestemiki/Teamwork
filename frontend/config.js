4f37400325bf6fb484426396e4dcc7171452fd09
Nyeste Miki <nyeste.miki@yahoo.com>
2020-03-16 00:39:02 +0200

    frontend cleanup

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,2 @@
-// This is client side config only - don't put anything in here that shouldn't be public!
+// Client-side config only
 export const endpoint = `http://localhost:4444`;
-export const perPage = 4;
