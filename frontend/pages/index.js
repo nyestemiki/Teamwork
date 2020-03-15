64cf848d6ef6351148122acb8078c1eea7abe8d6
Nyeste Miki <nyeste.miki@yahoo.com>
2020-03-16 00:52:26 +0200

    bare frontend structure

---
+++ frontend/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { injectGlobal } from 'styled-components';
+
+injectGlobal``;
+
+class Home extends React.Component {
+    render() {
+        return (
+            <h1>HEEEy</h1>
+        );
+    }
+}
+
+export default Home;
